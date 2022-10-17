ed0a0b1060e299dc420c5cbf900aa64c1da7bcc4
malgtps12 <93052305+malgtps12@users.noreply.github.com>
2022-10-17 19:44:24 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 ┌─「 Donasi • Pulsa 」
-│ • *Indosat:* [${global.ppulsa}]
+│ • *Tri:* '089516353968
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
-│ • *Dana:* [${global.pdana}]
+│ • *Dana:* '089516353968'
 ❏────
 `
 
