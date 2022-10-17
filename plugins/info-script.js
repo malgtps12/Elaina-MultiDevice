0d151e41583eb12fae30b6dff62cdad9b70535e7
malgtps12 <93052305+malgtps12@users.noreply.github.com>
2022-10-17 19:53:47 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -8,7 +8,7 @@ let str = `*Sorry Bro Script Gua Privatein*`
 let wibu = `-` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/Xiao_yan_21",
+    mediaUrl: "https://Instagram.com/xzdk.m",
     title: 'Malbot-MD',
     body: 'want source code?',
     thumbnail: thumb,
